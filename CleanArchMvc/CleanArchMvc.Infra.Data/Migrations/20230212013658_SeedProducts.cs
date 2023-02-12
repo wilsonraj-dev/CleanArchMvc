9ead24a1eb91bb883f83f9c524ce5b0fed8e31ab
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-11 22:46:50 -0300

    Applying EF Core Migrations

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Migrations/20230212013658_SeedProducts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+                   "VALUES ('Caderno espiral', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno1.jpg', 1)");
+
+            mb.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId) " +
+                   "VALUES ('Borracha', 'Borracha branca pequena', 3.45, 150, 'borracha1.jpg', 1)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
