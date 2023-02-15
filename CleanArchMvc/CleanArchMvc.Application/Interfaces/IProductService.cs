c97d9e06ae179a5e127e6dc17b141abe8c7c9c18
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-14 22:54:03 -0300

    CQRS - Adjusting the ProductService class using Mediator

--- CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc/CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -8,7 +8,6 @@ namespace CleanArchMvc.Application.Interfaces
     {
         Task<IEnumerable<ProductDTO>> GetProducts();
         Task<ProductDTO> GetById(int? id);
-        Task<ProductDTO> GetProductCategory(int? id);
         Task Add(ProductDTO productDTO);
         Task Update(ProductDTO productDTO);
         Task Remove(int? id);
