a01aa51e3486e508d1c6b0daf771c960c50948fe
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-15 23:05:18 -0300

    Displaying details of a category

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -100,5 +100,19 @@ namespace CleanArchMvc.WebUI.Controllers
             return RedirectToAction("Index");
         }
         #endregion
+
+        #region Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var categoryDTO = await _categoryService.GetById(id);
+            if (categoryDTO == null)
+                return NotFound();
+
+            return View(categoryDTO);
+        }
+        #endregion
     }
 }
