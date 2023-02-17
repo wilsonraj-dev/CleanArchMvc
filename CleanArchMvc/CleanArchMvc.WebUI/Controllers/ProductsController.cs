a3deb7d468a19c145fae61a2961dc84418f51078
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-16 22:22:26 -0300

    Displaying product details

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -10,11 +12,13 @@ namespace CleanArchMvc.WebUI.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService, ICategoryService categoryService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productService;
             _categoryService = categoryService;
+            _environment = environment;
         }
 
         #region Index
@@ -92,10 +96,29 @@ namespace CleanArchMvc.WebUI.Controllers
         }
 
         [HttpPost(), ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int? id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
             await _productService.Remove(id);
             return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productDTO = await _productService.GetById(id);
+            if (productDTO == null)
+                return NotFound();
+
+            var wwwrot = _environment.WebRootPath;
+            var image = Path.Combine(wwwrot, "images\\" + productDTO.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            return View(productDTO);
         }
         #endregion
     }
