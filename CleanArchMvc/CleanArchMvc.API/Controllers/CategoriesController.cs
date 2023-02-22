b913906ebdb2982ea88d401d5b0ac1a8b83f0d74
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 20:12:25 -0300

    HttPost - Creating a new category

--- CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -30,7 +30,7 @@ namespace CleanArchMvc.API.Controllers
             return Ok(categories);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetCategory")]
         public async Task<ActionResult<CategoryDTO>> Get(int id)
         {
             var category = await _categoryService.GetById(id);
@@ -41,6 +41,19 @@ namespace CleanArchMvc.API.Controllers
             }
 
             return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            await _categoryService.Add(categoryDTO);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.Id }, categoryDTO);
         }
     }
 }
