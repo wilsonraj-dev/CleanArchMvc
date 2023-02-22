6538fa1edfd4c32b17b3a0f2677acf31ce3169b2
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 20:05:11 -0300

    HttpGet{id} - Returning the category by its Id

--- CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -29,5 +29,18 @@ namespace CleanArchMvc.API.Controllers
 
             return Ok(categories);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+
+            return Ok(category);
+        }
     }
 }
