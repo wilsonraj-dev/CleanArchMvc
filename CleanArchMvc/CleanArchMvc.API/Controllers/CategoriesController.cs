2f4f005b02886a7291f46c29a077b391bca94411
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 20:01:46 -0300

    HttpGet - Returning all categories

--- CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
 {
@@ -6,5 +10,24 @@ namespace CleanArchMvc.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+
+            if (categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+
+            return Ok(categories);
+        }
     }
 }
