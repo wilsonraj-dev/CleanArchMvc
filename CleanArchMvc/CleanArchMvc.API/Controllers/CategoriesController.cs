3111fa41c3bf9f54025acc83e98da5d19885a3d9
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 19:47:27 -0300

    Creating CategoriesController

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
