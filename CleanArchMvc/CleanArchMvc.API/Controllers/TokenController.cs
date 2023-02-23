7d4ccd80929a78f9eafa2a86a1be070d7791d0ca
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 22:11:41 -0300

    Creating the method to generate the JWT Token

--- CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
@@ -1,7 +1,12 @@
 ﻿using CleanArchMvc.API.Models;
 using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
@@ -11,10 +16,12 @@ namespace CleanArchMvc.API.Controllers
     public class TokenController : ControllerBase
     {
         private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
 
-        public TokenController(IAuthenticate authenticate)
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
         {
             _authenticate = authenticate ?? throw new ArgumentNullException(nameof(authenticate));
+            _configuration = configuration;
         }
 
         [HttpPost("LoginUser")]
@@ -24,14 +31,48 @@ namespace CleanArchMvc.API.Controllers
 
             if (result)
             {
-                return Ok();
-                //return GenerateToken(userInfo);
+                return GenerateToken(userInfo);
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return BadRequest(ModelState);
             }
+        }
+
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+            //User declarations
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meuValor", "QualquerCoisa"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //Generate private key to sign token
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            //Generate the digital signature
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //Set the expiration time
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //Generate the token
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+                );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration,
+            };
         }
     }
 }
