57d0da7bd9c670f7be0d359535b85a0820d5b047
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-02-22 22:42:44 -0300

    Enabling authentication and validating the JWT token

--- CleanArchMvc/CleanArchMvc.API/Startup.cs
+++ CleanArchMvc/CleanArchMvc.API/Startup.cs
@@ -1,17 +1,10 @@
 using CleanArchMvc.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.API
 {
@@ -28,6 +21,10 @@ namespace CleanArchMvc.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructureAPI(Configuration);
+
+            //Enable authentication and validate token
+            services.AddInfrastructureJWT(Configuration);
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
